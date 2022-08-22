c9c05077824a502c6f0187619755c63339d47b61
AlexaGTorres <alexa.torres19122694@gmail.com>
2022-08-22 12:13:36 -0700

    First and second screen.

---
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.myfirstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickToast (View view)
+    {
+        Toast.makeText(this, "Next Page! :D", Toast.LENGTH_LONG).show();
+    }
+}
