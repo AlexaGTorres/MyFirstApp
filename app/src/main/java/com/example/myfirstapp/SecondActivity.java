71d38c6a13ba40831ce0ca0ad9577dbfe223c544
AlexaGTorres <alexa.torres19122694@gmail.com>
2022-08-22 12:25:27 -0700

    Working back screen button.

---
+++ app/src/main/java/com/example/myfirstapp/SecondActivity.java
@@ -0,0 +1,25 @@
+package com.example.myfirstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class SecondActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+    public void onClickBack (View view)
+    {
+        Toast.makeText(this, "Next Page! :D", Toast.LENGTH_LONG).show();
+        startActivity(new Intent(SecondActivity.this, MainActivity.class));
+    }
+}
