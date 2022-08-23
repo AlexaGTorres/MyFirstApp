696f24b2315cc15fdd9fc8e08b43fb77ea3daae8
AlexaGTorres <alexa.torres19122694@gmail.com>
2022-08-23 12:16:29 -0700

    Added logcat.

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -22,5 +23,6 @@ public class MainActivity extends AppCompatActivity
     {
         Toast.makeText(this, "Next Page! :D", Toast.LENGTH_LONG).show();
         startActivity(new Intent(MainActivity.this, SecondActivity.class));
+        Log.i("info", "Logcat thing I don't remember.");
     }
 }
